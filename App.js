36251c49b617d30e9d05395c48e3958a5ba81e16
maggieaschneider <margaret.anne.schneider@gmail.com>
2022-04-02 10:48:47 -0400

    ok

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <h1>Current Time</h1>
+      <h1>bdsjnds,nvkdjf</h1>
       <StatusBar style="auto" />
     </View>
   );
